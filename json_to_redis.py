97268cf0848a54785a41a4698841867e2ed1718e
anm237 <npmahale15@gmail.com>
2021-05-01 10:02:49 +0530

    Added python docstrings and comment

--- json_to_redis.py
+++ json_to_redis.py
@@ -10,10 +10,20 @@ docker run -d -p 6379:6379 --name redis-rejson redislabs/rejson:latest
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def write_json_redis(arr, json_file): 
+    """
+    Write a json file to redis.
+
+    Args:
+        arr (string): Key in redis that points to the JSON
+        json_file (string): JSON file read as string
+    """
     r.execute_command('JSON.SET', arr, '.', json_file)
     
 
 def read_from_redis_json():
+    """
+    Reading the JSON from redis as Python dictionary.
+    """
     reply = json.loads((r.execute_command('JSON.GET', 'doc')).decode('utf-8'))
 
     user1_age=reply['user1']['age']
@@ -26,6 +36,9 @@ def read_from_redis_json():
     print(user2_location_1)
 
 def update_json_to_redis():
+    """
+    Update operation using python dictionary. Dictionary is converted back to JSON strong.
+    """
     reply = json.loads((r.execute_command('JSON.GET', 'doc')).decode('utf-8'))
     reply['user1']['cars']['car2']='Honda'
     updated_dict_to_json=json.dumps(reply)
@@ -33,6 +46,9 @@ def update_json_to_redis():
     read_from_redis_json()
 
 def write_json_array():
+    """
+    Writing an array object to redis. It is read as Python list.
+    """
     sample_array='[ true, { "answer": 42 }, null ]'
     write_json_redis('arr',sample_array)
     reply = json.loads((r.execute_command('JSON.GET', 'arr')).decode('utf-8'))
@@ -44,16 +60,20 @@ def write_json_array():
 
 
 if __name__=='__main__':
-
+    #Opening and reading a local file
     with open('nested_sample.json','r') as f:
         json_file=f.read()
-        
+    
+    #Function call to write to redis   
     write_json_redis('doc', json_file)
 
+    #Function call read sample fields from redis
     read_from_redis_json()
 
+    #Fucntion call to update sample fields in redis
     update_json_to_redis()
 
+    #Function call to write an array to redis
     write_json_array()
 
 
--- redis_hash.py
+++ redis_hash.py
@@ -11,44 +11,81 @@ Sample JSON file used:
 
 '''
 def write_to_redis(json_file):
+  """
+  Write a JSON file to redis hash. Data is uploaded as Python dictionary
+
+  Args:
+      json_file (string): JSON file read as string
+  """
   redis_data=json_file
   redis_data_dict=json.loads(redis_data) 
   r.hmset('sample1',redis_data_dict)
 
 def read_from_redis(list1):
+  """
+  Read sample values with keys passed as Python list
+
+  Args:
+      list1 (list): List of sample keys to retrieve corresponding values
+  """
   redis_keys= r.keys()
   print(redis_keys)
   value=r.hmget('sample1',list1[0],list1[1])
   print(value) #list
 
 def update_to_redis(dict1):
+  """
+  Update sample fields to redis hash. Once updated read that field.
+
+  Args:
+      dict1 (dictionary): A new dictionary to be updated in redis hash
+  """
   r.hmset('sample1', dict1)
   value=r.hmget('sample1','country')
   print(value)
 
 def delete_from_redis(delete_key):
+  """
+  Delete a particular key in redis hash
+
+  Args:
+      delete_key (string): Key to be deleled from the redis hash
+  """
   r.delete('sample1', delete_key)
   print(r.hmget('sample1', delete_key))
 
 def delete_hash_from_redis(delete_hash):
+  """
+  Delete the entire hash object from redis
+
+  Args:
+      delete_hash (string): Name of the hash to be deleted
+  """
   r.delete(delete_hash)
   print(r.hmget(delete_hash,'name'))
 
 if __name__ == '__main__':
   
+  #Open and read a local json file
   with open('sample.json','r') as f:
     json_file=f.read()
+
+  #Function call to write the file to redis hash
   write_to_redis(json_file)
 
+  #Function to call read values of sample list of keys
   list1=['name','age']
   read_from_redis(list1)
-
+ 
+  #Function call to update a redis hash with sample dictionary
   dict1={'country':'India'}
   update_to_redis(dict1)
 
+  #Function call to delete a key in redis hash
   delete_key='country'
   delete_from_redis(delete_key)
  
+  #Function call to delete entire hash in redis
   delete_hash='sample1'
   delete_hash_from_redis(delete_hash)
 
